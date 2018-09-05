0ad72b70e2118a3d18c31be4480aa728539d7691
Pedro Lucas <pedrolucas499@gmail.com>
2018-09-05 16:56:29 -0300

    BancoConnection

---
+++ PostgreCRUD/BancoConnection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace ConsoleApp1
+{
+
+    class BancoConnection
+    {
+
+        private const String SERVER = "localhost";
+        private const String PORT = "5432";
+        private const String USER = "postgres";
+        private const String PASSWORD = "123456";
+        private const String DATABASE = "filmedb";
+
+        private NpgsqlConnection connection = null;
+
+        public BancoConnection()
+        {
+            connection = new NpgsqlConnection(
+                "Server=" + SERVER + ";" +
+                "Port=" + PORT + ";" +
+                "User Id=" + USER + ";" +
+                "Password=" + PASSWORD + ";" +
+                "Database=" + DATABASE + ";");
+        }
+
+        public NpgsqlConnection getConnection { get => connection; set => connection = value; }
+
+        public void OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+            }
+            catch (NpgsqlException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public void CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch (NpgsqlException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+    }
+}
